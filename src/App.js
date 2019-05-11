32e11ba9aef7681b0d5a45c5267c4c9bb720a7ff
igorMori <igor_canada@outlook.com>
2019-05-11 03:41:03 -0400

    s
    s

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Address from './body/address/address'
 import Type from './body/appraisalType/type'
 import Company from './body/appraisalCompany/company'
 import Details from './body/orderDetails/details' 
-import Body from './body/body'
+
 
 function App() {
   return (
@@ -19,7 +19,6 @@ function App() {
         <Route path="/type" component={Type}/>
         <Route path="/company" component={Company}/>
         <Route path="/details" component={Details}/>
-        <Route path="/submit" component={Body}/>
         <Route path="/" component={Address}/>
         
       </Switch>
